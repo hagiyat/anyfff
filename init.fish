f52c5629f16fa9a660f225a2090eb2cafdc8243a
hagiyat <hagiyat@gmail.com>
2017-01-26 14:21:05 +0900

    Change: package manager fisherman -> fundle

--- init.fish
+++ init.fish
@@ -1,4 +1,8 @@
+# dependencies
+fundle plugin 'fisherman/git_util'
+
 # environments {{{
+# finder application
 set -l ANYFFF__FINDER_COLLECTION sk peco fzf percol
 if not set -q ANYFFF__FINDER_APP
   for f in $ANYFFF__FINDER_COLLECTION
@@ -22,21 +26,26 @@ end
 # 3days
 set -x ANYFFF__CDR_CACHE_LIFETIME 3
 
+# chucksum command
 if test -x sha256sum
   set -x ANYFFF__CHECKSUM_APP 'sha256sum'
 else
   set -x ANYFFF__CHECKSUM_APP 'shasum -a 256'
 end
 
+# cache file for cd history
 set -x ANYFFF__CDR_HISTORIES_FILE history.log
 set -x ANYFFF__CDR_HISTORIES_PATH "$ANYFFF__CDR_CACHE_PATH/$ANYFFF__CDR_HISTORIES_FILE"
 touch $ANYFFF__CDR_HISTORIES_PATH
 
+# When this environment variable is enabled,
+# cd history reference and generation log will be outputted
 # set -x ANYFFF__CDR_ACTIVITY_PATH "$ANYFFF__CDR_CACHE_PATH/activity.log"
 if set -q ANYFFF__CDR_ACTIVITY_PATH
   touch $ANYFFF__CDR_ACTIVITY_PATH
 end
 
+# marks
 set -x ANYFFF__CDR_HISTORIES_MARK '-'
 set -x ANYFFF__CDR_SIBLINGS_MARK ':'
 set -x ANYFFF__CDR_BRANCHES_MARK '>'
@@ -47,6 +56,3 @@ if not set -q ANYFFF__FILESEARCH_MAXDEPTH
   set -x ANYFFF__FILESEARCH_MAXDEPTH 2
 end
 # }}}
-
-function anyfff -d "fuzzy finder wrapper plugin for fish"
-end
