fdd254cb8f37e4288b679904938083f92c8136b1
ryotako <kobayashiryota2046@gmail.com>
2017-02-25 17:56:39 +0900

    Fix: change filter’s option -p into —prompt

--- functions/__anyfff_finder.fish
+++ functions/__anyfff_finder.fish
@@ -3,10 +3,10 @@
 # multiple select> __anyfff_finder -m 'prompt message[multi] > '
 function __anyfff_finder -a single_or_multi prompt
   if [ $single_or_multi = '-s' ]
-    eval "$ANYFFF__FINDER_APP -p '$prompt'"
+    eval "$ANYFFF__FINDER_APP --prompt '$prompt'"
 
   else
-    eval "$ANYFFF__FINDER_APP -p '$prompt' $ANYFFF__FINDER_APP_OPTION_MULTIPLE" \
+    eval "$ANYFFF__FINDER_APP --prompt '$prompt' $ANYFFF__FINDER_APP_OPTION_MULTIPLE" \
       | __anyfff_util last \
       | xargs
   end
